f81ff051d54e9a2383f2f8d3f7ddabba600fe24b
u97jain <47736180+u97jain@users.noreply.github.com>
2022-06-06 21:07:30 -0400

    Time: 86 ms (81.05%), Space: 38.8 MB (62.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cs
+++ 242-valid-anagram/242-valid-anagram.cs
@@ -1,10 +1,48 @@
 public class Solution {
     public bool IsAnagram(string s, string t) 
     {
-        var sA = s.ToCharArray();
-        var tA = t.ToCharArray();
-        Array.Sort(sA);
-        Array.Sort(tA);
-        return new string(sA) == new string(tA);
+        /*
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+        */
+        
+        if (s.Length != t.Length) return false;
+        var dict = new Dictionary<char, int>();
+        
+        for(int i=0; i<s.Length; i++)
+        {
+            if(dict.ContainsKey(s[i]))
+            {
+                dict[s[i]]++;
+            }
+            else
+            {
+                dict.Add(s[i], 1);
+            }
+        }
+        
+        for(int j=0; j<t.Length; j++)
+        {
+            if(dict.ContainsKey(t[j]) && dict[t[j]]>0)
+            {
+                dict[t[j]]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
