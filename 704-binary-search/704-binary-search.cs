71f44ffd26471b425e9095fa2df0e0ccb446a930
u97jain <47736180+u97jain@users.noreply.github.com>
2022-06-07 00:34:53 -0400

    Time: 189 ms (26.89%), Space: 41.5 MB (63.71%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int Search(int[] nums, int target) 
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        
+        while(left<=right)
+        {
+            int mid = (left+right)/2;
+            
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(target>nums[mid])
+            {
+                left = mid+1;
+            }
+            else if(target<nums[mid])
+            {
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
