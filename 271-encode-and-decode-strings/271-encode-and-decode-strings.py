3ac96ce31916a1a672e56181624629d303f4cce0
u97jain <47736180+u97jain@users.noreply.github.com>
2022-06-20 01:35:02 -0400

    Time: 118 ms (34.10%), Space: 14.5 MB (18.82%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -13,9 +13,7 @@ class Codec:
         res, i = [], 0
         
         while i < len(s):
-            j = i
-            while s[j] != "#":
-                j += 1
+            j = s.find('#', i)
             length = int(s[i:j])
             res.append(s[j+1 : j+1+length])
             i = j + 1 + length
